a88e6c9faa04162473b9b2936058879155e63eb6
EyaOuenniche <eya.ouenniche@medtech.tn>
2025-02-27 00:53:56 +0100

    research lab form ui done

---
+++ src/app/CreateResearchLab/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import ResearchLabForm from "./components/ResearchLabForm";
+
+export default function ResearchLabCreation() {
+  return <ResearchLabForm />;
+}
